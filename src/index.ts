79f87c85cc1c0d8c6c3e3d32e1876d2f6b3f4f24
Bhargav Nookala <nooknb@gmail.com>
2018-12-10 14:22:25 -0800

    Adding ts stubs

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import * as dominos from 'dominos';
+
+const app = express()
+
+app.get('/capacity', function (req, res) {
+    res.send('capacity')
+});
+
+app.get('/nodeAddresses', function (req, res) {
+    res.send('node addresses')
+});
+
+app.get('/nodeConditions', function (req, res) {
+    res.send('node conditions')
+});
+
+app.get('/getPods', function (req, res) {
+    res.send('get pods')
+});
+
+app.get('/getPodStatus', function (req, res) {
+    res.send('pod status');
+});
+
+app.get('/getContainerLogs', function (req, res) {
+    res.send('get logs');
+});
+
+app.get('/getPod', function (req, res) {
+    res.send('get pod');
+});
+
+app.delete('/deletePod', function (req, res) {
+    res.send('delete pod');
+});
+
+app.put('/updatePod', function (req, res) {
+    res.send('update pod');
+});
+
+app.post('createPod', function (req, res) {
+    res.send('create pod');
+});
+
+app.listen(3000, () => { console.log("im aliveeeeee!"); });
