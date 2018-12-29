5a9a2358f81b607cd8bd6a80aa337c831ce87e5b
Bhargav Nookala <nooknb@gmail.com>
2018-12-29 14:21:17 -0800

    non interactive login

--- script/docker_push.sh
+++ script/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" -p $DOCKER_PASSWORD
 
 docker tag virtual-pizza $DOCKER_USERNAME/virtual-pizza
 docker push $DOCKER_USERNAME/virtual-pizza
